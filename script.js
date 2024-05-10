f9d18dbb4bf9e2b6a563756e1fae1d65cb847f74
Ravidas Gaikwad <ravidasmg@gmail.com>
2024-05-10 17:58:17 +0530

    My first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function readMore(){
+    let popup=document.getElementById("pop-up");
+    popup.style.display= 'block';
+    console.log("Hello Ji")
+}
+function closeButton(){
+    let popup=document.getElementById("pop-up");
+    popup.style.display= 'none';
+}
